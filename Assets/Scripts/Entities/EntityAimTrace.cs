18729960bd80194fc10d591f469071539ac0f2e2
najdorf <70437537+cn7249@users.noreply.github.com>
2024-01-23 09:41:07 +0900

    Initial commit

---
+++ Assets/Scripts/Entities/EntityAimTrace.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityAimTrace : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private GameCharacterController _controller;
+
+    void Awake()
+    {
+        _controller = GetComponent<GameCharacterController>();
+    }
+
+    void Start()
+    {
+        _controller.OnLookEvent += OnAim;
+    }
+
+    public void OnAim(Vector2 aimDirection)
+    {
+        FlipSprite(aimDirection);
+    }
+
+    private void FlipSprite(Vector2 direction)
+    {
+        float theta = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (Mathf.Abs(theta) > 90)
+        {
+            characterRenderer.flipX = true;
+        }
+        else
+        {
+            characterRenderer.flipX = false;
+        }
+    }
+    
+}
